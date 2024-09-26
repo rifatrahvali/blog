512e1e121c2bf57c79cfdc10c4f296a9c53c44fd
Rıfat Rahvalı <rifatrahvali@outlook.com>
2024-09-27 02:43:51 +0300

    28 - Makale tablosu ve modeli oluşturuldu

--- database/migrations/2024_09_26_232624_create_articles_table.php
+++ database/migrations/2024_09_26_232624_create_articles_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,31 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger("category_id");
+            $table->unsignedBigInteger("user_id");
+
+            $table->string("name", 80);
+            $table->string("slug", 160);
+
+            $table->text("body");
+            $table->string("image", 150)->nullable();
+            $table->boolean("status")->default(0);
+
+            $table->string("tags")->nullable();
+            $table->string("seo_description")->nullable();
+            $table->string("seo_keywords")->nullable();
+
+            $table->integer("read_time")->default(0);
+            $table->integer("view_count")->default(0);
+            $table->integer("like_count")->default(0);
+
+            $table->dateTime("publish_date");
             $table->timestamps();
+
+            // bu tablodaki category_id kolonu
+            // categories tablodaki id kolonuna referans alır
+            $table->foreign("category_id")->references("id")->on("categories")->onDelete("cascade");
+            $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
         });
     }
 
