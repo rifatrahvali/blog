0378d26b99053df9995624fe17453ce7f52e54de
Rıfat Rahvalı <rifatrahvali@outlook.com>
2024-09-27 02:26:59 +0300

    28 - kategori tablosu için migration yapıldı

---
+++ database/migrations/2024_09_26_231030_create_categories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 60);
+            $table->string("slug", 150);
+            $table->string("description", 250)->nullable();
+            $table->unsignedBigInteger("parent_id")->nullable();
+            $table->boolean("status")->default(0);
+            $table->boolean("feature_status")->default(0);
+            $table->integer("order")->default(0);
+            $table->string("seo_description")->nullable();
+            $table->string("seo_keywords")->nullable();
+            $table->timestamps();
+
+            // foreign key alan parent_id'ye karşılık gelecek
+            // referans id'den
+            $table->foreign("parent_id")->references("id")->on("categories")->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
