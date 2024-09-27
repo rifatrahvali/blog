f2e3f40c5682a34f63c20d9d122b7f19cba77d08
Rıfat Rahvalı <rifatrahvali@outlook.com>
2024-09-27 14:35:36 +0300

    30 - Veritabanı tabloları hazırlandı - foreign keyler kuruldu.

---
+++ database/migrations/2024_09_27_104609_create_user_like_articles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_like_articles', function (Blueprint $table) {
+            $table->unsignedBigInteger("user_id");
+            $table->unsignedBigInteger("article_id");
+
+            $table->foreign("user_id")->on("users")->references("id")->onDelete("cascade");
+            $table->foreign("article_id")->on("articles")->references("id")->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_like_articles');
+    }
+};
---
+++ database/migrations/2024_09_27_105148_create_article_comments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('article_comments', function (Blueprint $table) {
+            $table->id();
+            
+
+            $table->unsignedBigInteger("user_id");
+            $table->unsignedBigInteger("article_id");
+            $table->unsignedBigInteger("parent_id")->nullable();
+
+
+            $table->text("comment");
+            $table->tinyInteger("status")->default(0);
+            $table->integer("like_count")->default(0);
+            $table->integer("unlike_count")->default(0);
+
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign("parent_id")->on("article_comments")->references("id");
+            $table->foreign("article_id")->on("articles")->references("id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('article_comments');
+    }
+};
